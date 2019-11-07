7302f80e3aecc5718afb73fd3653abf30be25876
esmaeil mirzaee <esmaeilmirzaee@gmail.com>
2019-11-06 19:58:47 -0500

    more practise

--- adventure_two.rb
+++ adventure_two.rb
@@ -3,4 +3,19 @@ class Thing
     @name = name
     @description = description
   end
+  def set_name(name)
+    @name = name
+  end
+  def get_name
+    @name
+  end
+end
+
+class Treasure < Thing
+  def initialize(value)
+    @value = value
+  end
+  def set_value(value)
+    @value = value
+  end
 end
