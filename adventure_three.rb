57eb41ef9b3bc290543c52bab74ef718e1954c38
esmaeil mirzaee <esmaeilmirzaee@gmail.com>
2019-11-07 19:00:21 -0500

    setter and getter

--- adventure_three.rb
+++ adventure_three.rb
@@ -1,4 +1,23 @@
-class Treasure
-  def initialize
+class Thing
+  attr_reader :name
+  attr_reader :description
+  attr_writer :name
+  attr_writer :description
+  def initialize(name, description)
+    @name = name
+    @description = description
   end
 end
+
+class Treasure < Thing
+  attr_reader :value
+  attr_writer :value
+  def initialize(value)
+    @value = value
+  end
+end
+
+aThing = Thing.new
+aThing.name = "Samuel"
+aThing.description = "A developer"
+
