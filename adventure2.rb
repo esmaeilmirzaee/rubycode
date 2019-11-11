4d6975f3de2fc9405a7974613bcd133e241d7447
esmaeil mirzaee <esmaeilmirzaee@gmail.com>
2019-11-11 11:14:31 -0500

    class variables section

---
+++ adventure2.rb
@@ -0,0 +1,46 @@
+# illustrates how to creating decendent objects
+# reading and writing attributes
+# object (inatance) variables
+# class variables
+
+class Thing
+  @@num_things = 0
+
+  attr_reader( :name, :description)
+  attr_writer( :description)
+
+  def initialize(name, description)
+    @name = name
+    @description = description
+    @@num_things += 1
+  end
+
+  def to_s
+    "(Thing.to_s):: The #{@name} Thing is #{@description}."
+  end
+
+  def show_classvars
+    return "There are #{@@num_things} Thing objects in this game"
+  end
+end
+
+class Room < Thing
+  # TODO: Add Room-specific behaviour
+end
+
+class Treasure < Thing
+  attr_reader :value
+  attr_writer :value
+
+  def initialize(name, description, value)
+    super(name, description)
+    @value = value
+  end
+end
+
+class Map
+  def initialize(room)
+    @room = room
+  end
+  
+end
