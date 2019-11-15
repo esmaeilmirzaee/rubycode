5ef210f7bba5f039b9a6df98c716c5d8ae1b1732
esmaeil mirzaee <esmaeilmirzaee@gmail.com>
2019-11-15 03:48:51 -0500

    Keys and values in hash

--- hash_two.rb
+++ hash_two.rb
@@ -6,4 +6,13 @@ hash__two = {'ket1'=>'val1', 'key2'=>'val2', 'key3'=>'val3', 'key4'=>'val4'}
 puts 'keys...'
 print "hash_one's keys: "
 p hash_one.keys
+print "hash_two's keys: "
+p hash_two.keys
+puts 'values...'
+print "hash_one's values: "
+p hash_one.value
+print "hash_two's values: "
+p hash_two.value
+puts
+p hash_one.keys & hash_two.keys
 
