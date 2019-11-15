671f6fc49e7b73901376ab2fc4981fa1e74ea48d
esmaeil mirzaee <esmaeilmirzaee@gmail.com>
2019-11-15 03:56:20 -0500

    Keys and values in hash

--- hash_two.rb
+++ hash_two.rb
@@ -1,7 +1,7 @@
 # Accessing and using hashes via keys and values
 
 hash_one = {'key1'=>'val1', 'key2'=>'val2', 'key3'=>'val3', 'key4'=>'val4'}
-hash__two = {'ket1'=>'val1', 'key2'=>'val2', 'key3'=>'val3', 'key4'=>'val4'}
+hash_two = {'ket1'=>'val1', 'key2'=>'val2', 'key3'=>'val3', 'key4'=>'val4'}
 
 puts 'keys...'
 print "hash_one's keys: "
@@ -10,9 +10,15 @@ print "hash_two's keys: "
 p hash_two.keys
 puts 'values...'
 print "hash_one's values: "
-p hash_one.value
+p hash_one.values
 print "hash_two's values: "
-p hash_two.value
+p hash_two.values
 puts
 p hash_one.keys & hash_two.keys
-
+p hash_one.values & hash_two.values
+p hash_one.keys + hash_two.keys
+p hash_one.values - hash_two.values
+p hash_one.keys << hash_two.keys
+p (hash_one.keys << hash_two.keys)
+p (hash_one.keys << hash_two.keys).flatten
+p (hash_one.keys << hash_two.keys).flatten.reverse
