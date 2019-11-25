f298ace59097289757821a8c568f98763ca07c23
esmaeil mirzaee <esmaeilmirzaee@gmail.com>
2019-11-25 10:03:36 -0500

    class and object

---
+++ tt/back_account.rb
@@ -0,0 +1,17 @@
+class BankAccount
+  def initialize(name)
+    @transactions = []
+    @balance = 0
+  end
+
+  def deposit
+    print "How much would you like to deposit? "
+    amount = gets.chomp
+    @balance += amount.to_f
+    puts "$#{amount} deposited."
+  end
+
+  def show_balance
+    puts "Your balance is $#{@balance}."
+  end
+end
