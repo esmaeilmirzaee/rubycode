0a6c610343c24540741c354ffdd3cd99074f6307
esmaeil mirzaee <esmaeilmirzaee@gmail.com>
2019-12-01 14:53:52 -0500

    Progress 32

---
+++ bp/number_methods.rb
@@ -0,0 +1,20 @@
+#!/bin/ruby env
+
+p 10.9.to_i
+p 10.9.to_i.class
+
+p 10.5.floor
+p 10.4.floor
+p 10.6.floor
+
+p 10.5.ceil
+p 10.4.ceil
+p 10.6.ceil
+
+
+p 3.14159.round(2)
+p 3.14159.round(3)
+p 3.14159.round(4)
+p 3.14.round
+p 3.15.round
+p 3.16.round(4)
