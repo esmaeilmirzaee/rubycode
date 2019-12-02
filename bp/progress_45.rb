a51f1a832b20c12136b80ad8f33ebc7f64953270
esmaeil mirzaee <esmaeilmirzaee@gmail.com>
2019-12-02 06:07:08 -0500

    Progress 47

--- bp/progress_45.rb
+++ bp/progress_45.rb
@@ -6,3 +6,24 @@ p story[0]
 p story[4]
 
 p story.slice(10)
+
+p story[5, 4]
+p story.slice(5, 4)
+
+p story[0, 10]
+p story.slice(0, 10)
+
+another_story = story
+p another_story
+p another_story.object_id
+p story
+p story.object_id
+story = "I have changed the story."
+p another_story
+p story
+p story.object_id
+
+p story(-5, 7)
+p story[-1]
+
+p story[23..34]
