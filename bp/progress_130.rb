af2d0da8c70e8b7cd82e13a90171e7fe38cb5605
esmaeil mirzaee <esmaeilmirzaee@gmail.com>
2019-12-08 04:15:29 -0500

    Progress 134

--- bp/progress_130.rb
+++ bp/progress_130.rb
@@ -1,4 +1,4 @@
-# frozen_string_literal: true
+# frozen_string_literal: false
 fact = 'I am genius and hard-working.'
 
 p fact.index('e')
@@ -14,5 +14,39 @@ def custom_index(string, substring)
   length = substring.length
   string.chars.each_with_index do |char, index|
     sequence = string[index, length]
+    return index if sequence == substring
   end
 end
+
+
+typo = "GeorgWashington"
+p typo.dup.insert(5, 'e ')
+
+puts "Thhe aardvark jummmped    over tttthe fence!!!".squeeze
+
+# FIXME: returns only the first character in string
+def another_custom_squeeze(string)
+  final = ""
+  chars = string.split("")
+  chars.each_with_index do |char, index|
+    final << char[index] unless char[index] == char[index+1]
+  end
+  final
+end
+
+def custom_squeeze(string)
+  final = ""
+  chars = string.split("")
+  chars.each_with_index do |char, index|
+    char == chars[index + 1] ? next : final << char
+  end
+  final
+end
+
+p custom_squeeze("Thhe aardvark jummmped    over tttthe fence!!!")
+
+puts "*" * 10
+word = "goodbye"
+p word
+word.clear
+p word
