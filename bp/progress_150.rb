80ef538d0466e63a1d4e0f5fccd297de1eb2a3ac
esmaeil mirzaee <esmaeilmirzaee@gmail.com>
2019-12-10 12:02:29 -0500

    progress 158

--- bp/progress_150.rb
+++ bp/progress_150.rb
@@ -50,5 +50,42 @@ p "age".to_sym.class
 p "School Bus".to_sym
 p "=" * 20
 
+menu = {burger: 3.99, taco: 5.96, chips: 0.5}
+p menu[:burger]
+p menu[:chips]
+p menu[:salad]
+p menu.fetch(:burger)
+p menu.fetch(:salad, "Not defined")
+menu.store(:sushi, 24.99)
+menu.store(:steak, 34.99)
+p menu
+p "=" * 20
 
+shopping_list = {banana: 5, oranges: 10, carrots: 3, crackers: 15}
+vegan_shopping_list = {}
+p shopping_list.length
+p vegan_shopping_list.length
+p shopping_list.empty?
+p vegan_shopping_list.empty?
+p "=" * 20
+
+capitals = {alabama: "Montgomery",
+            alaska: "Juneau",
+            arizona: "Phoenix",
+            arkansas: "Little Rock"}
+
+capitals.each do |state, capital|
+  puts "Querying hash..."
+  puts  "The capital of #{state} is #{capital}!"
+end
+
+capitals.each do |pair|
+  p pair
+end
+
+capitals.each do |pair|
+  p pair[0]
+end
+p "=" * 20
+p "=" * 20
 p "=" * 20
