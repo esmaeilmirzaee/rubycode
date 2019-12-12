ea37aea365e87fcee46a9c4dd93e5961ad1b4537
esmaeil mirzaee <esmaeilmirzaee@gmail.com>
2019-12-12 17:48:22 -0500

    progress 176

--- bp/progress_170.rb
+++ bp/progress_170.rb
@@ -112,3 +112,49 @@ speak_the_truth_three("Boris") do |name, age|
   p age
   puts "#{name} is #{age} years old."
 end
+
+def number_evaluation(num_one, num_two, num_three)
+  puts "Inside the method."
+  yield(num_one, num_two, num_three)
+  # puts "Back inside the method!"
+end
+
+product = number_evaluation(5, 10, 15) { |n1, n2, n3| n1 * n2 * n3 }
+p product
+
+sum = number_evaluation(5, 10, 15) { |n1, n2, n3| n1 + n2 + n3 }
+p sum
+def custom_each(array)
+  i = 0
+  until i >= array.length
+    p array[i]
+    yield array[i]
+    i += 1
+  end
+end
+
+names = ["Boris", "Arnold", "Melissa"]
+numbers = [10, 20, 30]
+
+custom_each(names) do |name|
+  puts "The length of #{name} is #{name.length}!"
+end
+custom_each(numbers) do |number|
+  puts "The square of #{number} is #{number ** 2}."
+end
+
+def greeter
+  puts "I'm inside the greeter method."
+  yield
+end
+
+phrase = Proc.new do
+  puts "Inside the proc."
+end
+
+# phrase = {puts "Inside the parenthesis."} # impossible
+greeter(&phrase)
+
+hi = Proc.new { puts "Hi there"}
+5.times(&hi)
+5.times { hi.call }
