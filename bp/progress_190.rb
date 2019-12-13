e49bc135cef1286e3a45aaf3fc7c5cf3d75262ed
esmaeil mirzaee <esmaeilmirzaee@gmail.com>
2019-12-13 16:20:53 -0500

    progress 197

--- bp/progress_190.rb
+++ bp/progress_190.rb
@@ -19,3 +19,32 @@ File.open("./end.rb", "a") do |file|
 end
 require "./end.rb" # It does not work
 load "./end.rb"
+
+require_relative "../rubyconf2k19/nil_array_return"
+
+puts
+
+phrase = "The Programming Language is amazing!"
+puts phrase.start_with?("T")
+puts phrase.start_with?("t")
+puts phrase.end_with?("!")
+puts phrase.end_with?("zing")
+
+def custom_start_with?(string, substring)
+  string[0, substring.length] == substring
+end
+
+def custom_end_with?(string, substring)
+  string[-substring.length..-1] == substring # TODO: NOT WORKING
+end
+
+custom_start_with?(phrase, "The")
+custom_start_with?(phrase, "the")
+custom_end_with?(phrase, "!")
+custom_end_with?(phrase, "ing!")
+p phrase[-"zing!".length..-1] == "zing!"
+
+puts
+
+puts phrase.include?("programming")
+puts phrase.upcase.include?("PROGRAMMING")
