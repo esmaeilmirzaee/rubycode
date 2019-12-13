c9eb8ad6e12f2da2d64a51e027c2612e7d405474
esmaeil mirzaee <esmaeilmirzaee@gmail.com>
2019-12-13 04:33:35 -0500

    progress 180

--- bp/progress_170.rb
+++ bp/progress_170.rb
@@ -219,3 +219,39 @@ end
 p diet_p
 result = diet_p
 p result
+
+def convert_to_euros(dollars)
+  dollars * 0.95 if dollars.is_a?(Numeric)
+end
+
+def convert_to_pesos(dollars)
+  dollars * 20.67 if dollars.is_a?(Numeric)
+end
+
+def convert_to_rupess(dollars)
+  dollars * 68.13 if dollars.is_a?(Numeric)
+end
+
+p convert_to_euros(1000)
+p convert_to_pesos(1000)
+p convert_to_rupess(1000)
+
+def convert(dollars, currency)
+  yield(dollars, currency) if dollars.is_a?(Numeric)
+end
+
+p convert(1000, "euros") { |dollars| dollars * 0.95 }
+p convert(1000, "pesos") { |dollars| dollars * 20.67 }
+p convert(1000, "rupees") { |dollars| dollars * 68.13 }
+
+to_euros = lambda { |dollars| dollars * 0.95 }
+to_pesos = lambda { |dollars| dollars * 20.70 }
+to_rupees = lambda { |dollars| dollars * 68.13 }
+
+def convert_(dollars, currency_lambda)
+  currency_lambda.call(dollars) if dollars.is_a?(Numeric)
+end
+p convert_(1000, to_euros)
+p convert_(1000, to_pesos)
+p convert_(1000, to_rupees)
+p [1000, 2000, 3000].map(&:to_rupees)
