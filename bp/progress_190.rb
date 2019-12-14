3e8c903164337ade7486557ff978ec471e6b78d9
esmaeil mirzaee <esmaeilmirzaee@gmail.com>
2019-12-13 19:31:03 -0500

    progress 200

--- bp/progress_190.rb
+++ bp/progress_190.rb
@@ -24,7 +24,7 @@ require_relative "../rubyconf2k19/nil_array_return"
 
 puts
 
-phrase = "The Programming Language is amazing!"
+phrase = "The Ruby Programming Language is amazing!"
 puts phrase.start_with?("T")
 puts phrase.start_with?("t")
 puts phrase.end_with?("!")
@@ -48,3 +48,40 @@ puts
 
 puts phrase.include?("programming")
 puts phrase.upcase.include?("PROGRAMMING")
+
+seach_for = "Language"
+def custom_include?(string, substring)
+  len = substring.length
+  string.chars .each_with_index do |char, index|
+    string[index, len] == substring ? true : false
+  end
+end
+p custom_include?(phrase, seach_for)
+
+puts
+
+puts //.class
+puts phrase =~ /T/
+puts phrase =~ /R/
+puts phrase =~ /m/
+puts /m/ =~ phrase
+# puts /!/ =~ phrase
+# puts phrase =~ / /
+p phrase =~ /x/ # puts does not print nil, rather it prints just a blank line.
+puts phrase =~ /anguage/
+
+puts
+
+voicemail = "I can be reached at 555-123-4567 or send email to regexman@gmail.com."
+p voicemail.scan(/e/) # provides an array
+p voicemail.scan(/re/)
+p voicemail.scan(/[re]/)
+
+puts
+
+p voicemail.scan(/d/)
+p voicemail.scan(/\d/)
+p voicemail.scan(/\d+/)
+p voicemail.scan(/\D/).count
+p voicemail.scan(/\s/).length
+p voicemail.scan(/\S/).length
