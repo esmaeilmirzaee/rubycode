167d16d96e6f2ac4fdabdde4996c8f402e230162
esmaeil mirzaee <esmaeilmirzaee@gmail.com>
2019-12-14 14:14:58 -0500

    progress 208

--- bp/progress_200.rb
+++ bp/progress_200.rb
@@ -20,3 +20,72 @@ p lambda {}.class
 p Time.new.class
 
 puts
+
+p 5.class
+p 5.class.superclass
+p 5.class.superclass.superclass
+p 5.class.superclass.superclass.superclass
+p 5.class.superclass.superclass.superclass.superclass
+
+p 5.2.class
+p 5.2.class.superclass
+p 5.2.class.superclass.superclass
+p 5.2.class.superclass.superclass.superclass
+p 5.2.class.superclass.superclass.superclass.superclass
+
+puts
+
+p [1, 2, "a"].class
+p [1, 2, "a"].class.superclass
+p [1, 2, "a"].class.superclass.superclass
+p [1, 2, "a"].class.superclass.superclass.superclass
+
+puts
+
+p "".class
+p "".class.superclass
+p "".class.superclass.superclass
+p "".class.superclass.superclass.superclass
+
+puts
+
+p Hash.new.class
+p Hash.new.class.superclass
+p Hash.new.class.superclass.superclass
+p Hash.new.class.superclass.superclass.superclass
+
+puts
+
+a_symbol = :something
+p a_symbol.class
+p a_symbol.class.superclass
+p a_symbol.class.superclass.superclass
+p a_symbol.class.superclass.superclass.superclass
+
+puts
+
+# This is a ancestor
+p 5.class.ancestors
+p 5.3.class.ancestors
+p "".class.ancestors
+p Array.new.class.ancestors
+p Hash.new.class.ancestors
+a_symbol = :something
+p a_symbol.class.ancestors
+
+puts
+
+int_methods = 1.methods.sort
+float_methods = 2.2.methods.sort
+p int_methods - float_methods
+
+arr_methods = [1, 2, 3].methods.sort
+hash_methods = {key: "value"}.methods.sort
+p arr_methods - hash_methods
+
+puts
+class Thing << String
+  def to_s
+
+  end
+end
