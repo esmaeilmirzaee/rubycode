5d5ba732190ba4be69dc78b579f393f2bb08a3c5
esmaeil mirzaee <esmaeilmirzaee@gmail.com>
2019-12-14 15:33:47 -0500

    progress 212

--- bp/progress_210.rb
+++ bp/progress_210.rb
@@ -2,6 +2,9 @@ class Gadget
 
 end
 
+puts Gadget.superclass
+puts Gadget.superclass.superclass
+
 phone = Gadget.new
 laptop = Gadget.new
 microwave = Gadget.new
@@ -9,3 +12,47 @@ microwave = Gadget.new
 puts phone
 puts laptop
 puts microwave
+
+puts phone.methods.sort
+# puts phone.method(:trust)
+
+puts phone.is_a?(Gadget)
+puts phone.is_a?(BasicObject)
+puts phone.is_a?(Object)
+puts phone.respond_to?(:respond_to?)
+puts phone.respond_to?(:length)
+puts phone.respond_to?(:methods)
+puts phone.respond_to?(:is_a?)
+
+puts
+
+puts phone.object_id
+puts laptop.object_id
+
+puts phone == laptop
+puts phone.object_id == laptop.object_id
+
+shiny = phone
+
+p shiny.object_id
+p shiny.object_id == laptop.object_id
+p shiny == laptop
+
+puts
+puts
+
+class Gadget
+  def initialize
+    @username = "User #{rand(1..100)}"
+    @password = "topsecret"
+    @production_number = "#{("a".."z").to_a.sample}-#{rand(1..999)}"
+  end
+end
+
+phone = Gadget.new
+p phone.inspect
+p phone.instance_variables
+
+
+laptop = Gadget.new
+p laptop.instance_variables
