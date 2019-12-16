3cb051612698244604ba959697986a0adf4eecf0
esmaeil mirzaee <esmaeilmirzaee@gmail.com>
2019-12-16 02:33:26 -0500

    progress 224

---
+++ bp/progress_224.rb
@@ -0,0 +1,26 @@
+module Purchaseable
+  def purchase(item)
+    "#{item} has been purchased."
+  end
+end
+
+class Bookstore
+  include Purchaseable
+end
+
+class Supermarket
+  include Purchaseable
+end
+
+# Inheritance
+class CornerMart < Supermarket
+
+end
+barnes_and_noble = Bookstore.new
+p barnes_and_noble.purchase("Atlas Shrugged")
+
+shoprite = Supermarket.new
+p shoprite.purchase("Ice cream.")
+
+quickstop = CornerMart.new
+p quickstop.purchase("Slim Jim")
