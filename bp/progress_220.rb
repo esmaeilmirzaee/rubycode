20c120a0786bde230bfe5bbae2b8cfa4af734ca9
esmaeil mirzaee <esmaeilmirzaee@gmail.com>
2019-12-15 22:13:30 -0500

    progress 221

--- bp/progress_220.rb
+++ bp/progress_220.rb
@@ -1,7 +1,25 @@
 module LengthConversions
 
-  def self.miles_to_feet(miles)
+  WEBSITE = "https://google.com"
 
+  def self.miles_to_feet(miles)
+    miles * 5280
   end
 
+  def self.miles_to_inches(miles)
+    feet = miles_to_feet(miles)
+    feet * 12
+  end
+
+  def self.miles_to_centimeters(miles)
+    inches = miles_to_inches(miles)
+    inches * 2.54
+  end
 end
+
+puts LengthConversions::WEBSITE
+# puts LengthConversions.WEBSITE undefined method `WEBSITE` for Lengthconversions:Module
+
+puts LengthConversions.miles_to_feet(100)
+puts LengthConversions.miles_to_inches(200)
+puts LengthConversions.miles_to_centimeters(300)
