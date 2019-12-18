7ce8e58c4204a6e66f1e81ae28aacd409f4e73b9
esmaeil mirzaee <esmaeilmirzaee@gmail.com>
2019-12-18 16:29:41 -0500

    progress 250

--- bp/album.rb
+++ bp/album.rb
@@ -1,15 +1,22 @@
+# frozen_string_literal: false
+# An Album class the stores an array of songs
 class Album
   include Enumerable
+
+  # An array object of songs. Each song is a string.
   attr_reader :songs
 
+  # Creates a new Album instance with an empty songs.
   def initialize
     @songs = []    
   end
 
+  # Add a song to the Album object's songs array.
   def add_song(song)
     songs << song    
   end
 
+  # Yield each in song in the album to a block.
   def each
     songs.each do |song|
       yield song      
