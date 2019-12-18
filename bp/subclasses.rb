eb8fb4073594f02bae20e3c871690ca80b564c52
esmaeil mirzaee <esmaeilmirzaee@gmail.com>
2019-12-18 06:56:26 -0500

    progress 244

--- bp/subclasses.rb
+++ bp/subclasses.rb
@@ -16,11 +16,20 @@ boris = Employee.new("Boris", 25)
 puts boris.introduce
 
 class Manager < Employee
-  
+  def yell
+    'Who\'s the boss? I\'m the boss!'
+    
+  end
 end
 
 class Worker < Employee
-  
+  def clock_in(time)
+    "Starting my shift at #{time}."
+  end
+
+  def yell
+    "I'm working! I'm working!"    
+  end
 end
 
 bob = Manager.new("Bob", 48)
@@ -35,3 +44,27 @@ p Worker < Manager
 puts Worker < Manager
 puts Worker == Manager
 p Worker.ancestors
+
+puts bob.is_a?(Manager)
+puts bob.is_a?(Worker)
+puts bob.is_a?(Employee)
+puts bob.is_a?(Object)
+puts bob.is_a?(Kernel)
+puts bob.is_a?(BasicObject)
+
+puts
+
+puts bob.instance_of?(Employee)
+puts bob.instance_of?(Manager)
+puts bob.instance_of?(Object)
+puts bob.instance_of?(Kernel)
+puts bob.instance_of?(BasicObject)
+
+puts
+
+p bob.introduce
+p dan.introduce
+
+p bob.yell
+p dan.clock_in('8.30')
+p dan.yell
